39bcf2e7fce1eef1c75075de05c6e0f4cd642157
fredxhansson-cmyk <fredxhansson@gmail.com>
2026-06-24 16:23:43 +0200

    AXIOM: next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   generateEtags: true,
-  images: { domains: ['images-eu.ssl-images-amazon.com','images-na.ssl-images-amazon.com','m.media-amazon.com'] },
+  images: { domains: ['axiom-engine-production-54c3.up.railway.app'] },
   async headers() {
     return [{ source: '/(.*)', headers: [
-      { key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' data: https://images-eu.ssl-images-amazon.com https://images-na.ssl-images-amazon.com https://m.media-amazon.com https://logo.clearbit.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cloud.umami.is; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://cloud.umami.is; frame-ancestors 'none'" },
+      { key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' data: https://axiom-engine-production-54c3.up.railway.app; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cloud.umami.is; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://cloud.umami.is; frame-ancestors 'none'" },
     ]}];
   },
 }
